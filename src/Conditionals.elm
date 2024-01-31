ab9ef012084a47118961160b033af22e9878a935
Chelsea Austine <austinechelsea7@gmail.com>
2024-01-31 16:06:27 +0100

    Add gifts for the twelve days of christmas

--- src/Conditionals.elm
+++ src/Conditionals.elm
@@ -84,7 +84,57 @@ ageClassifier age =
 
       else
         "Invalid age"
--- Any age outside these range should be ("Invalid age")        
+-- Any age outside these range should be ("Invalid age")  
+
+
+
+--This function twelveDaysOfChrismass is a case expression that takes an integer
+-- day as input and returns a string as an output.
+twelveDaysOfChrismass : Int -> String
+twelveDaysOfChrismass day =
+    case day of 
+
+        1 ->
+            "A Partridge in a Pear Tree"
+
+        2 ->
+            "Two Turtle Doves"
+
+        3 ->
+            "Three French Hens"
+
+        4 ->
+            "Four Calling Birds"
+
+        5 ->
+            "Five Golden Rings"
+
+        6 ->
+            "Six Geese a Laying"
+
+        7 ->
+            "Seven Swans a Swimming"
+
+        8 ->
+            "Eight Maids a Milking"
+
+        9 ->
+            "Nine Ladies Dancing"
+
+        10 ->
+            "Ten Lords a Leaping"
+
+        11 ->
+            "Eleven Pipers Piping"
+
+        12 ->
+            "Twelve Drummers Drumming"
+ -- The underscore (_) represents any other number
+ -- once you input numbers from 13, it will return "Invalide Day"
+ --
+        _ ->
+            "Invalid Day"
+
 
 
 
