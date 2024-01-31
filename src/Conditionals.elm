32552be4deeef11f1baa9c58146c6a9160c73882
Chelsea Austine <austinechelsea7@gmail.com>
2024-01-31 18:00:18 +0100

    Add 24 to 12 hour format converter

--- src/Conditionals.elm
+++ src/Conditionals.elm
@@ -278,6 +278,30 @@ trafficLightAction color =
 
 
 
+convertTo12HourFormat: Int -> (Int, String)
+convertTo12HourFormat hour =
+    if hour >= 0 && hour <= 23 then
+
+        let
+            (convertedHour, period) =
+                if hour < 12 then
+                    if hour == 0 then
+                        (12, "AM")
+                    else
+                        (hour, "AM")
+                else
+                    if hour == 12 then
+                        (12, "PM")
+                    else
+                        (hour - 12, "PM")
+        in
+        (convertedHour, period)
+    else
+        (-1, "Extra time? This isn't football, hun!")
+
+
+
+
 
 
 
