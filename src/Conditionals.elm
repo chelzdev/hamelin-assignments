ec576641fd3f1f3e6d83d1d34d7ddfc7aefe47f7
Chelsea Austine <austinechelsea7@gmail.com>
2024-01-31 13:51:28 +0100

    Add character Categorizer

--- src/Conditionals.elm
+++ src/Conditionals.elm
@@ -26,7 +26,31 @@ calculateGrade score =
 
         else 
             "Invalid Score"
---Anything else aside these range and a nagetive number should return "Invalide Score"           
+--Anything else aside these range and a nagetive number should return "Invalide Score" 
+
+
+
+categorizeCharacter : Char -> String
+categorizeCharacter char =
+    if char == 'a' || 
+       char == 'A' then
+        "Vowel"
+--This line checks if character 'A' is (uppercase or lowercase) 
+-- then it should return "Vowel"  which is a string.  
+    else if char == 'z' || char == 'Z' then
+        "Consonant"
+--This line checks if character 'Z' is (uppercase or lowercase) 
+-- then returns a string "Consonant" 
+    else if char == '3' then
+        "Digit"
+--This line checks if character is '3'  
+-- then  it returns a string "Digit"
+    else if char == '@' then
+        "Unknown"
+--This line checks if character that's not categorized or a symbols 
+-- like '@' then returns "Unknown"  as a string    
+     else   
+         "Unknown"
 
 
 
