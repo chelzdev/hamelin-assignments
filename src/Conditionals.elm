ab5b38d2d547d8a65431d1ba5efffc48fe358535
Chelsea Austine <austinechelsea7@gmail.com>
2024-01-31 17:15:51 +0100

    Add solution to convert number to month

--- src/Conditionals.elm
+++ src/Conditionals.elm
@@ -228,6 +228,29 @@ convertNumbersToMonth month =
 -- it will return  "Yeah, right, there's a 13th month!"       
 
 
+-- This function is case expression that
+-- takes an Integer and returns a String.
+
+convertNumberToMonth : Int -> String
+convertNumberToMonth month =
+    case month of
+  
+        1 -> "January"
+        2 -> "February"
+        3 -> "March"
+        4 -> "April"
+        5 -> "May"
+        6 -> "June"
+        7 -> "July"
+        8 -> "August"
+        9 -> "September"
+        10 -> "October"
+        11 -> "November"
+        12 -> "December"
+        _ -> "Yeah, right, there's a 13th month!"
+
+
+
 
 
 
